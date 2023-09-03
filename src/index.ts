771d596943a2705dd38c02c79875dce11b95d282
Robert Adams <adamsiii.robert@gmail.com>
2023-09-02 19:47:18 -0700

    add main fn

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,53 @@
-import 'dotenv/config';
-import { input } from '@inquirer/prompts';
-import { model } from './model';
-import { parser } from './parser';
-import { template } from './template';
+import "dotenv/config";
+import fsp from "fs/promises";
+import dayjs from "dayjs";
+import { input } from "@inquirer/prompts";
+import { model } from "./model";
+import { template } from "./template";
 
-(async () => {
+main();
+
+async function main() {
+  try {
+    await fsp.access("history", fsp.constants.F_OK);
+  } catch (error) {
+    console.log(error);
+    await fsp.mkdir("history");
+  }
+
   let loop = true;
   while (loop) {
     try {
-      const question = await input({ message: 'Ask a coding question:' });
+      const question = await input({ message: "Ask a coding question:" });
       switch (question) {
         default:
           const prompt = await template.format({ question });
           const output = await model.call(prompt);
-          const result = await parser.parse(output);
-          console.log(result);
+
+          if (!output) {
+            console.log("Hmmm...try rephrasing the question");
+            break;
+          }
+
+          const path = `history/question_${dayjs().format(
+            "YYYY_MM_DD-HH_mm_sss"
+          )}.md`;
+          await fsp.writeFile(path, output);
           break;
 
-        case '':
-          console.log('Please enter a coding question\n');
+        case "":
+          console.log("Please enter a coding question\n");
           break;
 
-        case 'q':
-        case 'quit':
-        case 'exit':
+        case "q":
+        case "quit":
+        case "exit":
           loop = false;
-          console.log('exiting...goodbye!');
+          console.log("exiting...goodbye!");
           break;
       }
     } catch (error) {
       console.log(error);
     }
   }
-})();
+}
