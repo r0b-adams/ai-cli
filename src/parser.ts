80eb9b3d4e41c5b15c8f66088d48840ec88868cb
Robert Adams <adamsiii.robert@gmail.com>
2023-09-02 19:46:47 -0700

    prettier

--- src/parser.ts
+++ src/parser.ts
@@ -1,6 +1,6 @@
-import { StructuredOutputParser } from 'langchain/output_parsers';
+import { StructuredOutputParser } from "langchain/output_parsers";
 
 export const parser = StructuredOutputParser.fromNamesAndDescriptions({
   code: "JavaScript code that answers the user's question",
-  explanation: 'detailed explanation of the example code provided',
+  explanation: "detailed explanation of the example code provided",
 });
